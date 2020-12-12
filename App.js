d62871d8a0dcd10358f31851a8269000b8cc8e3b
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2020-12-12 12:00:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import {createAppContainer} from 'react-navigation';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <AppContainer/>
     </View>
   );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppContainer = createAppContainer({
+  LoginScreen : {screen : LoginScreen}
+})
+
+
+
